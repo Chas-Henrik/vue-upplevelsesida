304a08df2603217b89f6f30967d9122934ebeabf
Niklas-Nordin <niklas.nordin@chasacademy.se>
2025-11-25 15:01:58 +0100

    A function for showing total price value

--- app/pages/confirmation.vue
+++ app/pages/confirmation.vue
@@ -1,5 +1,25 @@
+<script setup>
+  import { useCartStore } from '~/stores/cart';
+  import { computed } from 'vue';
+
+  const cartStore = useCartStore();
+
+  const bookings = computed(() => cartStore.items);
+
+  const totalPrice = computed(() =>
+  bookings.value.reduce((total, booking) => {
+    return total + booking.bookingFields.reduce((fieldTotal, field) => {
+      const offerSum = field.selectedOffers.reduce((offerTotal, offer) => offerTotal + offer.price, 0);
+        return fieldTotal + offerSum + field.excursionPrice
+      }, 0);
+    }, 0)
+  ); 
+  
+</script>
+
 <template>
   <div>
     <h1>Confirmation</h1>
+    <p>{{ totalPrice }} kr</p>
   </div>
 </template>
